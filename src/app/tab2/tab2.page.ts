222a0cfc7a57a498cdaea745d4897bed600838fd
Diego <diego@Diegos-Laptop.local>
2023-07-03 16:56:27 -0500

    feature: search for movies

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,12 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+  textoBuscar='';
+
   constructor() {}
+
+  search(event:any){
+    const valor = event.detail.value;
+  }
 
 }
